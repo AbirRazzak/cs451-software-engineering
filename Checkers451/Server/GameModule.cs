0514c8b3832eefeba52555adc0da7e68339263af
Abir Razzak <amr439@drexel.edu>
2019-08-01 12:46:32 -0400

    Created Project Solution
    Created project for Server, project for Client still needs to be added

---
+++ Checkers451/Server/GameModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nancy;
+
+namespace Server
+{
+    public class GameModule : NancyModule
+    {
+        public GameModule()
+        {
+            Get("/", x => {
+                Console.WriteLine("Someone has connected.");
+                return "Hello World.";
+            });
+
+            Get("/greet/{name}", x => {
+                Console.WriteLine("Hi " + x.name);
+                return string.Concat("Hello ", x.name);
+            });
+        }
+    }
+}
---
+++ Checkers451/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace Server
+{
+    class Program
+    {
+        private static readonly String exitCode = "exit()";
+
+        static void Main(string[] args)
+        {
+            String consoleInput;
+            var uri = new Uri("http://localhost:55555/");
+            var config = new HostConfiguration { RewriteLocalhost = true };
+            var startupMessage = new StringBuilder("Running on ");
+            startupMessage.AppendFormat("{0}. Use {1} to shut down the server.", uri.ToString(), exitCode);
+            var host = new NancyHost(config, uri);
+            try
+            {
+                host.Start();
+                Console.WriteLine(startupMessage);
+
+                do
+                {
+                    consoleInput = Console.ReadLine();
+                } while (!consoleInput.Equals(exitCode));
+                host.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unknown error has occurred. See below: \n" + ex.ToString());
+            }
+        }
+    }
+}
