06102e6367caba73ad7ef459379dce520c20a722
Abir Razzak <amr439@drexel.edu>
2019-08-27 01:49:48 -0400

    test: Test Server Message and Move Routes

--- GamerServer.Test/GameModuleTest.cs
+++ GamerServer.Test/GameModuleTest.cs
@@ -22,11 +22,86 @@ namespace GameServer.Test
             });
 
             result.StatusCode.ShouldBe(HttpStatusCode.OK);
+            result.Body.AsString().ShouldBe("GameModule is online.");
+        }
+
+        [TestMethod]
+        public void TestMessageRoutes()
+        {
+            var browser = CreateNewDefaultGameModule();
+
+            // Test /msg logic
+            var msgSend = browser.Post("/msg/abc/Hello", with => {
+                with.HttpRequest();
+            });
+
+            msgSend.StatusCode.ShouldBe(HttpStatusCode.OK);
+            msgSend.Body.AsString().ShouldBe("Message sent.");
+
+            // Test /getlatestmsg logic
+            var msgRecieve = browser.Get("/getlatestmsg/abc", with => {
+                with.HttpRequest();
+            });
+
+            msgRecieve.StatusCode.ShouldBe(HttpStatusCode.OK);
+            msgRecieve.Body.AsString().ShouldBe("Hello");
+        }
+
+        [TestMethod]
+        public void TestMoveRoutes()
+        {
+            // Create a random gameID to prevent previous moves from messing up the test
+            var gameID = GenerateRandomString();
+            var browser = CreateNewDefaultGameModule();
+
+            // Test /move logic
+            var moveSend = browser.Post("/move/" + gameID +"/50,41", with => {
+                with.HttpRequest();
+            });
+
+            moveSend.StatusCode.ShouldBe(HttpStatusCode.OK);
+            moveSend.Body.AsString().ShouldBe("Move sent.");
+
+            // Test /getlatestmove logic
+            var moveLatest = browser.Get("/getlatestmove/" + gameID, with => {
+                with.HttpRequest();
+            });
+
+            moveLatest.StatusCode.ShouldBe(HttpStatusCode.OK);
+            moveLatest.Body.AsString().ShouldBe("50,41");
+
+            // Test /getlatestmove logic
+            browser.Post("/move/" + gameID + "/23,32", with => {
+                with.HttpRequest();
+            });
+
+            var moveAll = browser.Get("/getallmoves/" + gameID, with => {
+                with.HttpRequest();
+            });
+
+            moveAll.StatusCode.ShouldBe(HttpStatusCode.OK);
+            moveAll.Body.AsString().ShouldBe("50,41\r\n23,32\r\n");
         }
 
         private Browser CreateNewDefaultGameModule()
         {
             return new Browser(with => with.Module(new GameModule()));
+        }
+
+        private string GenerateRandomString()
+        {
+            // Code block from https://stackoverflow.com/a/1344258
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            return finalString;
         }
     }
 }
